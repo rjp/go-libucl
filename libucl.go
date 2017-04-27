ab819872e7241dd4db3d9607231529c381b4493b
Christopher Hall <hsw@bitmark.com>
2017-04-27 16:12:43 +0800

    convert to pkg-config
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- libucl.go
+++ libucl.go
@@ -1,9 +1,6 @@
 package libucl
 
-// #cgo CFLAGS: -Ivendor/libucl/include -Wno-int-to-void-pointer-cast
-// #cgo LDFLAGS: -Lvendor/libucl -lucl
-//
-// #cgo freebsd CFLAGS: -I/usr/local/include -Wno-int-to-void-pointer-cast
-// #cgo freebsd LDFLAGS: -L/usr/local/lib -lucl
+// #cgo CFLAGS: -Wno-int-to-void-pointer-cast
+// #cgo pkg-config: libucl
 //
 import "C"
